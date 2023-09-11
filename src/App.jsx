18a247b4fc13b63139b947f9f57bb2d8adb76d34
Alex Prapugicu <paxthedev@gmail.com>
2023-09-11 12:24:00 +0300

    barebone project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./tailwind.css";
+
+function App() {
+  return (
+    <div className="bg-gray-800 text-center w-full h-screen">
+      <h1>Interview Project </h1>
+    </div>
+  );
+}
+
+export default App;
